22744081c92e14ad793254e1ebfdc657709118ee
Dan Turner <dan.turner@york.ac.uk>
2015-06-12 08:24:09 +0100

    Couple extra HexEncoder tests.

--- src/test/java/org/turner/opie/utils/HexEncoderTest.java
+++ src/test/java/org/turner/opie/utils/HexEncoderTest.java
@@ -1,6 +1,7 @@
 package org.turner.opie.utils;
 
 import org.junit.Assert;
+import org.junit.experimental.theories.suppliers.TestedOn;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.junit.Test;
@@ -21,5 +22,17 @@ public class HexEncoderTest {
     public void testDecodeSimple() {
         byte[] encode = HexEncoder.decode("ff0001");
         Assert.assertArrayEquals(encode, new byte[]{~0x00, 0x00, 0x01});
+    }
+
+    @Test
+    public void testEncodeEmpty() {
+        String encode = HexEncoder.encode(new byte[0]);
+        Assert.assertEquals("", encode);
+    }
+
+    @Test
+    public void testDecodeEmpty() {
+        byte[] decode = HexEncoder.decode("");
+        Assert.assertArrayEquals(new byte[0], decode);
     }
 }
